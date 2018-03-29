f15da5ce36f68d204275b3cb0acb8966198eeb42
Joimar <Joimar@Joi>
2018-03-28 21:32:29 -0300

    teste2

--- Compilador20181/src/compilador20181/Compilador20181.java
+++ Compilador20181/src/compilador20181/Compilador20181.java
@@ -16,6 +16,7 @@ public class Compilador20181 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Pro inferno");
     }
     
 }
